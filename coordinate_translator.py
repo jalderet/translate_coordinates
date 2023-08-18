11a9958f1b42256f3516c5e4fbb2ec925618eb54
jalderet <137326135+jalderet@users.noreply.github.com>
2023-08-18 16:57:39 -0700

    Update coordinate_translator.py

--- coordinate_translator.py
+++ coordinate_translator.py
@@ -9,51 +9,52 @@ class TileMapTranslator:
         self.origin_y_2 = origin_y_2 
 
     def translate_coordinate(self, opti_x, opti_y):
+        
         relative_y = "Mat not set up correctly"
         area_number = "^^^^^^^^^^^^^^^^^^^^^^^"
 
         if self.origin_y_1 == self.origin_y_2 and self.origin_x_1 < self.origin_x_2: #mat pointing up
             if opti_x < self.origin_x_1 or opti_x > self.origin_x_2 or opti_y < self.origin_y_1: #makes sure object is on mat
                 relative_y = "Not in area"
-                area_number = "N/A1"
+                area_number = "N/A"
             else:
                 relative_y = abs(opti_y - self.origin_y_1)
                 area_number = int(relative_y / self.area_height) + 1  #section on the mat
-                print("1")
+                
 
         elif self.origin_y_1 == self.origin_y_2 and self.origin_x_1 > self.origin_x_2: #mat pointing down
             if opti_x < self.origin_x_1 or opti_x > self.origin_x_2 or opti_y > self.origin_y_1: #makes sure object is on mat
                 relative_y = "Not in area"
-                area_number = "N/A2"
+                area_number = "N/A"
             else:
                 relative_y = abs(opti_y - self.origin_y_1)
                 area_number = int(relative_y / self.area_height) + 1  #section on the mat
-                print("2")
+                
 
         elif self.origin_x_1 == self.origin_x_2 and self.origin_y_1 < self.origin_y_2: #mat pointing left
             if opti_x > self.origin_x_1 or opti_y > self.origin_y_2 or opti_y < self.origin_y_1: #makes sure object is on mat
                 relative_y = "Not in area"
-                area_number = "N/A3"
+                area_number = "N/A"
             else:
                 relative_y = abs(opti_x - self.origin_x_1)
                 area_number = int(relative_y / self.area_height) + 1 #section on the mat
-                print("3")
+                
 
         elif self.origin_x_1 == self.origin_x_2 and self.origin_y_1 > self.origin_y_2: #mat pointing right
             if opti_x < self.origin_x_1 or opti_y > self.origin_y_2 or opti_y < self.origin_y_1: #makes sure object is on mat
                 relative_y = "Not in area"
-                area_number = "N/A4"
+                area_number = "N/A"
             else:
                 relative_y = abs(opti_x - self.origin_x_1)
                 area_number = int(relative_y / self.area_height) + 1 #section on the mat
-                print("4")
+                
 
         return area_number, relative_y
 
-# Example usage
+# Example
 if __name__ == "__main__":
-    AREA_WIDTH = 0.146
-    AREA_HEIGHT = 0.288931
+    AREA_WIDTH = 0.146 
+    AREA_HEIGHT = 0.288931 # (11.6 in)
     origin_x_1 = -1.72  #bottom of mat bottom corner/middle (must be right most in lab)
     origin_y_1 = -1.478
     origin_x_2 = -1.574  #bottom of mat bottom corner/middle (must be left most in lab)
